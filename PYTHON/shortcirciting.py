0d7a3be35a0df3eadc591976731ea84a60f609a3
Hrushikesh Sunil Kadam <hrushikeshkdm7683@gmail.com>
2023-05-05 15:09:01 +0530

    2nd commit

--- PYTHON/shortcirciting.py
+++ PYTHON/shortcirciting.py
@@ -9,3 +9,7 @@ var3=False
 var4='who care\'s'  
 if var3 and var4 :
    print('If dada dont cares what is var4 as it founds var3 is false and there is \'and\' logical operator it booms(sp therefore this statement is not going to execute)')
+
+# This is all about Shortcircuiting 
+# quite easy concept
+#    
