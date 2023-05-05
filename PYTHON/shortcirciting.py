a944902892215c16beaca377414d95ad52c133f6
Hrushikesh Sunil Kadam <hrushikeshkdm7683@gmail.com>
2023-05-05 15:00:19 +0530

    1st commit

---
+++ PYTHON/shortcirciting.py
@@ -0,0 +1,11 @@
+# A simple concept
+
+var1=True
+var2='who care\'s'
+if var1 or var2 :
+    print('If dada dont cares what is var2 as it founds var1 is true and there is \'or\' logical operator it booms')
+
+var3=False
+var4='who care\'s'  
+if var3 and var4 :
+   print('If dada dont cares what is var4 as it founds var3 is false and there is \'and\' logical operator it booms(sp therefore this statement is not going to execute)')
