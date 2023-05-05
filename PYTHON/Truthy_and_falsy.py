7f2d3f83a1f25eb63f5914fb89ebab8fcd42e7de
Hrushikesh Sunil Kadam <hrushikeshkdm7683@gmail.com>
2023-05-05 14:47:16 +0530

    1st commit

---
+++ PYTHON/Truthy_and_falsy.py
@@ -0,0 +1,20 @@
+
+
+var1='mahesh'
+var2=89745
+if var1 :
+    print('a string which is not empty is always true')
+elif var2 :
+    print('An int or float which is not zero is always true')  
+
+var3=' '
+var4= 0
+if var3 :
+    print('Not going to execute')
+elif var4 :
+    print('Not going to execute')  
+else :
+    print('will execute')
+
+#This is all about truthy and falsy values in python       
+   
